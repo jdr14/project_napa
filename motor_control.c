3c7800bf6ef8e9462c664a9c615fe71be7561e1b
jdr14 <jdrodgers2020@gmail.com>
2025-01-09 06:38:56 +0000

    Extended the code to include right side drive train movement and also connected GPIOs to the 2nd H-bridge.

--- motor_control.c
+++ motor_control.c
@@ -22,17 +22,31 @@
 #define FL_MOTOR_PIN_A (23)
 #define FL_MOTOR_PIN_B (24)
 
+// These are the pins corresponding to the right side drive train
+#define BR_MOTOR_PIN_A (26)
+#define BR_MOTOR_PIN_B (27)
+#define FR_MOTOR_PIN_A (5)
+#define FR_MOTOR_PIN_B (6)
+
 // Define the Register Index which will be used to set the motor pins to output
 #define BL_RI_PIN_A (BL_MOTOR_PIN_A / GPIO_PINS_PER_REG)
 #define BL_RI_PIN_B (BL_MOTOR_PIN_B / GPIO_PINS_PER_REG)
 #define FL_RI_PIN_A (FL_MOTOR_PIN_A / GPIO_PINS_PER_REG)
 #define FL_RI_PIN_B (FL_MOTOR_PIN_B / GPIO_PINS_PER_REG)
+#define BR_RI_PIN_A (BR_MOTOR_PIN_A / GPIO_PINS_PER_REG)
+#define BR_RI_PIN_B (BR_MOTOR_PIN_B / GPIO_PINS_PER_REG)
+#define FR_RI_PIN_A (FR_MOTOR_PIN_A / GPIO_PINS_PER_REG)
+#define FR_RI_PIN_B (FR_MOTOR_PIN_B / GPIO_PINS_PER_REG)
 
 // Define the register set shifts
 #define BL_SHIFT_A ((BL_MOTOR_PIN_A % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
 #define BL_SHIFT_B ((BL_MOTOR_PIN_B % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
 #define FL_SHIFT_A ((FL_MOTOR_PIN_A % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
 #define FL_SHIFT_B ((FL_MOTOR_PIN_B % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
+#define BR_SHIFT_A ((BR_MOTOR_PIN_A % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
+#define BR_SHIFT_B ((BR_MOTOR_PIN_B % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
+#define FR_SHIFT_A ((FR_MOTOR_PIN_A % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
+#define FR_SHIFT_B ((FR_MOTOR_PIN_B % GPIO_PINS_PER_REG) * FSEL_NUM_BITS)
 
 // This Register will be used to set the pin high
 #define GPIO_SET_REG (OFFSET_GPSET0 / GPIO_REG_SIZE)
@@ -43,13 +57,19 @@
 // These are the masks bitwise or-ed with the set and clear registers to control the logicl level of the pins ultimately controlling polarity/direction of each motor
 // I am using macros to define these up here so these definitions can be resolved entirely during the preprocessor stage of compile time
 // saving us from using unnecessary cycles on an unchanging value
-#define LEFT_SIDE_FORWARD_SET_MASK  ( (1 << BL_MOTOR_PIN_A) | (1 << FL_MOTOR_PIN_A) ) // Forward
+#define LEFT_SIDE_FORWARD_SET_MASK  ( (1 << BL_MOTOR_PIN_A) | (1 << FL_MOTOR_PIN_A) ) // Left Side Forward
 #define LEFT_SIDE_FORWARD_CLR_MASK  ( (1 << BL_MOTOR_PIN_B) | (1 << FL_MOTOR_PIN_B) )
 
-#define LEFT_SIDE_BACKWARD_SET_MASK ( (1 << BL_MOTOR_PIN_B) | (1 << FL_MOTOR_PIN_B) ) // Backward
+#define LEFT_SIDE_BACKWARD_SET_MASK ( (1 << BL_MOTOR_PIN_B) | (1 << FL_MOTOR_PIN_B) ) // Left Side Backward
 #define LEFT_SIDE_BACKWARD_CLR_MASK ( (1 << BL_MOTOR_PIN_A) | (1 << FL_MOTOR_PIN_A) )
 
-#define STOP_ALL_MOTORS_MASK ( (1 << BL_MOTOR_PIN_A) | (1 << BL_MOTOR_PIN_B) | (1 << FL_MOTOR_PIN_A) | (1 << FL_MOTOR_PIN_B) ) // Stop
+#define RIGHT_SIDE_FORWARD_SET_MASK  ( (1 << BR_MOTOR_PIN_A) | (1 << FR_MOTOR_PIN_A) ) // Right Side Forward
+#define RIGHT_SIDE_FORWARD_CLR_MASK  ( (1 << BR_MOTOR_PIN_B) | (1 << FR_MOTOR_PIN_B) )
+
+#define RIGHT_SIDE_BACKWARD_SET_MASK ( (1 << BR_MOTOR_PIN_B) | (1 << FR_MOTOR_PIN_B) ) // Right Side Backward
+#define RIGHT_SIDE_BACKWARD_CLR_MASK ( (1 << BR_MOTOR_PIN_A) | (1 << FR_MOTOR_PIN_A) )
+
+#define STOP_ALL_MOTORS_MASK ((1<<BL_MOTOR_PIN_A) | (1<<BL_MOTOR_PIN_B) | (1<<FL_MOTOR_PIN_A) | (1<<FL_MOTOR_PIN_B) | (1<<BR_MOTOR_PIN_A) | (1<<BR_MOTOR_PIN_B) | (1<<FR_MOTOR_PIN_A) | (1<<FR_MOTOR_PIN_B))
 
 int handle_error(char * msg) 
 {
@@ -60,33 +80,50 @@ int handle_error(char * msg)
 int init_gpio_pins(volatile uint32_t * gpios) 
 {
     printf("\nClearing and initializing all motor GPIOs to output:");
-
+    
+    // Setup FR motor pin A and B
+    gpios[(uint32_t)FR_RI_PIN_A] |= (1 << FR_SHIFT_A) | (1 << FR_SHIFT_B); // Should be register 0
+    
     // Setup BL motor pin A
-    gpios[(uint32_t)BL_RI_PIN_A] |= (1 << BL_SHIFT_A);
+    gpios[(uint32_t)BL_RI_PIN_A] |= (1 << BL_SHIFT_A); // Should be register 1
 
-    // Setup BL motor pin B and FL motor pins A and B
+    // Setup BL motor pin B and FL motor pins A and B, and BR pins A and B
     // We can set these pins up at the same time because they are all controlled at different bit offsets within the same register
-    gpios[(uint32_t)BL_RI_PIN_B] |= (1 << BL_SHIFT_B) | (1 << FL_SHIFT_A) | (1 << FL_SHIFT_B);
+    gpios[(uint32_t)BL_RI_PIN_B] |= (1 << BL_SHIFT_B) | (1 << FL_SHIFT_A) | (1 << FL_SHIFT_B) | (1 << BR_SHIFT_A) | (1 << BR_SHIFT_B); // Should be register 2
 }
 
 void moveLeftSideForward(volatile uint32_t * gpios) 
 {
     printf("\nMoving Left Side Forward...");
-    gpios[GPIO_CLR_REG] = LEFT_SIDE_FORWARD_CLR_MASK; // (1 << BL_MOTOR_PIN_B);
-    gpios[GPIO_SET_REG] = LEFT_SIDE_FORWARD_SET_MASK; //(1 << BL_MOTOR_PIN_A); // Back left motor
+    gpios[GPIO_CLR_REG] = LEFT_SIDE_FORWARD_CLR_MASK;
+    gpios[GPIO_SET_REG] = LEFT_SIDE_FORWARD_SET_MASK;
+}
+
+void moveRightSideForward(volatile uint32_t * gpios) 
+{
+    printf("\nMoving Right Side Forward...");
+    gpios[GPIO_CLR_REG] = RIGHT_SIDE_FORWARD_CLR_MASK;
+    gpios[GPIO_SET_REG] = RIGHT_SIDE_FORWARD_SET_MASK;
 }
 
 void stop(volatile uint32_t * gpios) 
 {
     printf("\nStopping Left Side Drive Train...");
-    gpios[GPIO_CLR_REG] = STOP_ALL_MOTORS_MASK; // (1 << BL_MOTOR_PIN_A); // Back left motor
+    gpios[GPIO_CLR_REG] = STOP_ALL_MOTORS_MASK;
 }
 
 void moveLeftSideBackward(volatile uint32_t * gpios) 
 {
     printf("\nMoving Left Side Backward...");
-    gpios[GPIO_CLR_REG] = LEFT_SIDE_BACKWARD_CLR_MASK; // (1 << BL_MOTOR_PIN_B);
-    gpios[GPIO_SET_REG] = LEFT_SIDE_BACKWARD_SET_MASK; // (1 << BL_MOTOR_PIN_A); // Back left motor
+    gpios[GPIO_CLR_REG] = LEFT_SIDE_BACKWARD_CLR_MASK;
+    gpios[GPIO_SET_REG] = LEFT_SIDE_BACKWARD_SET_MASK;
+}
+
+void moveRightSideBackward(volatile uint32_t * gpios) 
+{
+    printf("\nMoving Right Side Backward...");
+    gpios[GPIO_CLR_REG] = RIGHT_SIDE_BACKWARD_CLR_MASK;
+    gpios[GPIO_SET_REG] = RIGHT_SIDE_BACKWARD_SET_MASK;
 }
 
 int main()
@@ -102,7 +139,7 @@ int main()
     // Man page: https://man7.org/linux/man-pages/man2/mmap.2.html
     volatile uint32_t * gpios = (volatile uint32_t *)mmap(
         NULL,                    // Allow kernel to decide the virtual base address
-        GPIO_BLOCK_SIZE, // This is the last register in the GPIO memory block
+        GPIO_BLOCK_SIZE,         // This is the size of the GPIO memory block
         PROT_READ | PROT_WRITE,  // Enable read and write access
         MAP_SHARED,              // Updates to mapping are visible by other processes, which is what we want with GPIO
         gpiomem_fd,              // FD we created above
@@ -115,7 +152,7 @@ int main()
     // Test...
     uint32_t count = 0;
     do {
-        moveLeftSideForward(gpios);
+        moveRightSideForward(gpios);
 
         sleep(1);
 
@@ -123,7 +160,7 @@ int main()
 
         sleep(1);
 
-        moveLeftSideBackward(gpios);
+        moveRightSideBackward(gpios);
 
         sleep(1);
         count++;
