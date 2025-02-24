438cb487bfe38b49e56545b725f193d1a134bcb7
Joey Rodgers <jdr14@vt.edu>
2025-02-23 21:28:41 -0500

    PWM still broken.  Wiringpi might not be the best library for this purpose since it was EOL-ed in 2023

--- main.cpp
+++ main.cpp
@@ -16,15 +16,19 @@
 #define FR_MOTOR_PIN_B (6)  // Pin 31
 
 // PWM pins
-#define BL_PWM0_0_PIN (12) // Pin 32 (Needs Alt func 0)
+// #define BL_PWM0_0_PIN (12) // Pin 32 (Needs Alt func 0)
 // #define BR_PWM1_1_PIN (13) // Pin 33 (Needs Alt func 0)
-#define BR_PWM1_1_PIN (1) // Pin 33 (Needs Alt func 0)
-#define FL_PWM0_0_PIN (18) // Pin 12 (Needs Alt func 5)
-#define FR_PWM1_1_PIN (19) // Pin 35 (Needs Alt func 5)
+// #define FL_PWM0_0_PIN (18) // Pin 12 (Needs Alt func 5)
+// #define FR_PWM1_1_PIN (19) // Pin 35 (Needs Alt func 5)
+#define BL_PWM0_0_PIN (32) // Pin 32 (Needs Alt func 0)
+#define BR_PWM1_1_PIN (33) // Pin 33 (Needs Alt func 0)
+#define FL_PWM0_0_PIN (12) // Pin 12 (Needs Alt func 5)
+#define FR_PWM1_1_PIN (35) // Pin 35 (Needs Alt func 5)
 
 void setup() 
 {
-    if (wiringPiSetup() == -1) {
+    // if (wiringPiSetup() == -1) {
+    if (wiringPiSetupGpio() == -1) {
         std::cerr << "Setup Failed" << std::endl;
         exit(1);
     }
@@ -51,7 +55,6 @@ void setup()
     pwmSetClock(32);
 }
 
-
 int main(void)
 {
     setup();
@@ -63,11 +66,13 @@ int main(void)
     digitalWrite(FL_MOTOR_PIN_A, HIGH);
     digitalWrite(FL_MOTOR_PIN_B, LOW);
 
-    digitalWrite(BR_MOTOR_PIN_A, HIGH);
-    digitalWrite(BR_MOTOR_PIN_B, LOW);
+    digitalWrite(BR_MOTOR_PIN_A, LOW);
+    digitalWrite(BR_MOTOR_PIN_B, HIGH);
     
     digitalWrite(FR_MOTOR_PIN_A, HIGH);
     digitalWrite(FR_MOTOR_PIN_B, LOW);
+    
+    // delay(100);
     
     int speed;
     for (int i = 0; i < 1; i++) {
@@ -76,14 +81,14 @@ int main(void)
             pwmWrite(BR_PWM1_1_PIN, speed);
             pwmWrite(FL_PWM0_0_PIN, speed);
             pwmWrite(FR_PWM1_1_PIN, speed);
-            delay(500);
+            delay(1);
         }
         for(speed = 1023; speed >= 0; --speed) {
             pwmWrite(BL_PWM0_0_PIN, speed);
             pwmWrite(BR_PWM1_1_PIN, speed);
             pwmWrite(FL_PWM0_0_PIN, speed);
             pwmWrite(FR_PWM1_1_PIN, speed);
-            delay(500);
+            delay(1);
         }
     }
     
