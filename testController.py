e2671708f4cdc93c476e12ad2f11de26d441e3f2
Joey Rodgers <jdr14@vt.edu>
2025-03-08 14:17:46 -0500

    Figured out the controller button mappings.  Finally connected a bluetooth controller through bluetoothctl and was able to determine the controller's button mappings through this test script.

---
+++ testController.py
@@ -0,0 +1,40 @@
+from evdev import InputDevice, categorize, ecodes
+from time import sleep
+
+# Find the device path. You might need to check /dev/input/event* for the correct one.
+controller = InputDevice('/dev/input/event4') # Replace X with the correct event number
+
+print(f"Successfully connected to {controller.name}")
+print(controller.phys)
+
+sleep(2)
+
+# Through trial and error of the following, I've been able to figure out the pro controller mappings
+# Button Pad
+# B  --> code = 304 | type = 1 | value = 1
+# A  --> code = 305 | type = 1 | value = 1
+# X  --> code = 307 | type = 1 | value = 1
+# Y  --> code = 308 | type = 1 | value = 1
+#
+# Triggers
+# L  --> code = 310 | type = 1 | value = 1
+# R  --> code = 311 | type = 1 | value = 1
+# XL --> code = 312 | type = 1 | value = 1
+# XR --> code = 313 | type = 1 | value = 1
+#
+# Joystick Button Press
+# Left  Joystick --> code = 317 | type = 1 | value = 1
+# RIght Joystick --> code = 318 | type = 1 | value = 1
+#
+# Volume buttons
+# - --> code = 314 | type = 1 | value = 1
+# + --> code = 315 | type = 1 | value = 1
+# 
+# Select and Home
+# Home   --> code = 316 | type = 1 | value = 1
+# Select --> code = 309 | type = 1 | value = 1
+
+for event in controller.read_loop():
+    if event.type == ecodes.EV_KEY:
+        print(f"type = {event.type} | code = {event.code} | value = {event.value}")
+        exit(0)
